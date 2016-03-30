c2f9a74a3413fb380e53b9a4623cfa08d57d5dc3
ericweidman <ericweidman@gmail.com>
2016-03-30 11:52:26 -0400

    Displays Still Ants

---
+++ src/sample/Ant.java
@@ -0,0 +1,14 @@
+package sample;
+
+/**
+ * Created by ericweidman on 3/30/16.
+ */
+public class Ant {
+    double x;
+    double y;
+
+    public Ant(double x, double y) {
+        this.x = x;
+        this.y = y;
+    }
+}
